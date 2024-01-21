a0cc8a70d9de7e64705cb740e5ada4a3e9675922
phai <git@github.com:Charak6823/spring-boot-postgreSQL-Demo.git>
2024-01-21 17:00:02 +0700

    first commit

---
+++ src/test/java/kh/edu/bbu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.bbu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
